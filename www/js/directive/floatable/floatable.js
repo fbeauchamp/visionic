b7e97143b1f90274bec08e768d2e6de3e1323a83
florent <fbeauchamp@sdis71.fr>
2015-10-19 15:29:16 +0200

    docking now behave as intended + code standard fix

--- www/js/directive/floatable/floatable.js
+++ www/js/directive/floatable/floatable.js
@@ -1,59 +1,57 @@
-
+var angular = window.angular
 angular
   .module('starter.directives')
-  .directive('floatable', function($ionicGesture) {
+  .directive('floatable', function ($ionicGesture) {
+    return {
+      restrict: 'A',
+      link: link
+    }
 
-  return {
-    restrict: 'A',
-    scope:{
-      float:'='
-    },
-    link: link,
-    //templateUrl:'js/directive/floatable/template.html'
-  };
-
-  function link(scope,element,attrs) {
-    var container = angular.element(element.children()[0])
-    var handleTap = function (e) {
-      console.log(' tapped')
-      scope.$apply(function(){
-        scope.float = !scope.float
-      })
-    };
-    var tapGesture = $ionicGesture.on('tap', handleTap, element);
-    scope.$on('$destroy', function () {
-      // Clean up - unbind drag gesture handler
-      $ionicGesture.off(tapGesture, 'tap', handleTap);
-    });
-
-    scope.$watch(function(){ return scope.float}, function (float) {
-      console.log('change', scope.float)
-      if(float) {
-        floatVideo(element)
-      }else {
-        dockVideo(element)
+    function link (scope, element, attrs) {
+      var index = parseInt(attrs.floatIndex, 10) || 1
+      var handleTap = function (e) {
+        scope.$apply(function () {
+          attrs.float = !attrs.float
+        })
       }
-    })
-  }
+      var tapGesture = $ionicGesture.on('tap', handleTap, element)
+      scope.$on('$destroy', function () {
+        // Clean up - unbind drag gesture handler
+        $ionicGesture.off(tapGesture, 'tap', handleTap)
+      })
 
-  function floatVideo(element){
-    element.css({
-      float:'right',
-      overflow:'hidden',
-      height: '70px',
-      width: '70px',
-      'border-radius': '50%',
-      border: '2px solid rgba(255,255,255,0.75)'
-    })
-  }
+      scope.$watch(function () { return attrs.float }, function (float) {
+        if (float) {
+          floatVideo(element)
+        }else {
+          dockVideo(element)
+        }
+        function floatVideo () {
+          // video should float above others content
+          element.addClass('floatable-and-floated')
+          element.css({
+            position: 'absolute',
+            overflow: 'hidden',
+            height: '70px',
+            width: '70px',
+            right: 0,
+            top: (80 * (index - 1)) + 'px',
+            'border-radius': '50%',
+            border: '2px solid rgba(255,255,255,0.75)'
+          })
+        }
 
-  function dockVideo(element){
-    element.css({
-      float:'left',
-      height: 'auto',
-      width: 'auto',
-      'border-radius': '0',
-      border: 'none'
-    })
-  }
-});
+        function dockVideo () {
+          element.removeClass('floatable-and-floated')
+          // video should take all available space in container
+          element.css({
+            position: '',
+            height: 'auto',
+            width: 'auto',
+            'border-radius': '0',
+            border: 'none'
+          })
+        }
+      })
+    }
+  })
